b17f76e21e31f2ad0c8e007386ca1dd15f2fe888
Michael O'Ryan <michael.oryan@gmail.com>
2020-11-19 15:51:03 +1100

    Tidy up HTMLCode file

--- src/util/htmlcodes.ts
+++ src/util/htmlcodes.ts
@@ -1,16 +1,24 @@
 enum HttpStatusCode {
-    
-    OK = 200,
+    // Informational
+    PROCESSING = 102,
 
+    // Success
+    OK = 200,
     CREATED = 201, // Return a location for the new resource
     ACCEPTED = 202, // Return a location for the pending resource
     
+    // Redirection
+
+    // Client Errors
     FORBIDDEN = 403,
     NOT_FOUND = 404,
 
+    // Server Errors
     INTERNAL_SERVER_ERROR = 500,
     NOT_IMPLEMENTED = 501,
     SERVICE_UNAVAILABLE = 503,
+
+    // Unofficial codes
 
 }
 
